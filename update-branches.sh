82854694e083e685804394b212590a800aeab3c9
Keidrych Anton-Oates <keidrych@tayloredtechnology.net>
2017-08-08 13:31:54 +1000

    Enable Branch Sync & Updates

---
+++ update-branches.sh
@@ -0,0 +1,12 @@
+# $1 is the branch that we are merging master into
+#if [ "$#" -ne 1 ]; then
+#  echo "Branch to merge master into must be specified" 
+#  exit 1
+#fi
+
+for branched in $(git for-each-ref refs/heads | cut -d/ -f3-); do
+  git checkout $branched
+  git checkout master -- ./$branched
+  git commit -am "Release Master Changes"
+  git checkout master
+done
